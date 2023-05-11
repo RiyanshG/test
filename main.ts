2d88041bb5ff4823aa5863266a4c931eeb979e38
RiyanshG <113317780+RiyanshG@users.noreply.github.com>
2023-05-11 13:12:41 -0700

    Finished Code

--- main.ts
+++ main.ts
@@ -12,6 +12,30 @@ function GoForward (sec: number) {
     hummingbird.setRotationServo(FourPort.One, 0)
     hummingbird.setRotationServo(FourPort.Two, 0)
 }
+function PoliceLEDOff () {
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    0,
+    0,
+    0
+    )
+}
+function PoliceLEDOn () {
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    255,
+    0,
+    0
+    )
+    basic.pause(300)
+    hummingbird.setTriLED(
+    TwoPort.Two,
+    0,
+    0,
+    255
+    )
+    basic.pause(300)
+}
 function GoBackward (sec: number) {
     hummingbird.setRotationServo(FourPort.One, -100)
     hummingbird.setRotationServo(FourPort.Two, 55)
@@ -26,12 +50,61 @@ function TurnRight (sec: number) {
     hummingbird.setRotationServo(FourPort.One, 0)
     hummingbird.setRotationServo(FourPort.Two, 0)
 }
+function Stop () {
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+}
 hummingbird.startHummingbird()
+let direction = 0
+hummingbird.setLED(ThreePort.One, 100)
+hummingbird.setLED(ThreePort.Two, 100)
 basic.forever(function () {
-    GoForward(3)
-    GoBackward(3)
-    TurnLeft(3)
-    GoForward(3)
-    GoBackward(3)
-    TurnRight(3)
+    if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 3.5) {
+        while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 3.5) {
+        	
+        }
+        while (hummingbird.getSensor(SensorType.Distance, ThreePort.One) > 3.5) {
+            direction = 1
+            GoForward(3)
+            direction = -1
+            GoBackward(3)
+            direction = -0.5
+            TurnLeft(3)
+            direction = 1
+            GoForward(3)
+            direction = -1
+            GoBackward(3)
+            direction = 0.5
+            TurnRight(3)
+        }
+    } else {
+        direction = 0
+        Stop()
+    }
+})
+basic.forever(function () {
+    if (!(direction == 0)) {
+        hummingbird.setLED(ThreePort.Three, 0)
+        hummingbird.setTriLED(
+        TwoPort.One,
+        0,
+        0,
+        0
+        )
+        PoliceLEDOn()
+        while (!(direction == 0)) {
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            music.playTone(622, music.beat(BeatFraction.Whole))
+        }
+    } else {
+        PoliceLEDOff()
+        music.stopAllSounds()
+        hummingbird.setLED(ThreePort.Three, 100)
+        hummingbird.setTriLED(
+        TwoPort.One,
+        255,
+        0,
+        0
+        )
+    }
 })
