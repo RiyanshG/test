e5f037a07a542915a20d875d795684e6cfce6039
RiyanshG <113317780+RiyanshG@users.noreply.github.com>
2023-05-24 21:10:47 -0700

    added spinning servo

--- main.ts
+++ main.ts
@@ -29,6 +29,7 @@ function PoliceLEDOn () {
         0,
         0
         )
+        hummingbird.setPositionServo(FourPort.Three, 0)
         music.playTone(523, music.beat(BeatFraction.Whole))
         hummingbird.setTriLED(
         TwoPort.Two,
@@ -36,6 +37,7 @@ function PoliceLEDOn () {
         0,
         255
         )
+        hummingbird.setPositionServo(FourPort.Three, 180)
         music.playTone(622, music.beat(BeatFraction.Whole))
     }
 }
@@ -57,13 +59,35 @@ function Stop () {
     hummingbird.setRotationServo(FourPort.One, 0)
     hummingbird.setRotationServo(FourPort.Two, 0)
 }
-// Start hummingbird and turn on the headlights
+// Start hummingbird and turn on the headlights:
+// 
+// direction: -1: backward, 0: stop, 1: forward, -0.5: left, 0.5: right
+// LED 1: left front yellow light
+// LED 2: right front yellow light
+// .................................................................................
+// The LEDs are:
+//    single:
+// 1: left front yellow light
+// 2: right front yellow light
+// 3: left back red light
+//     tri:
+// 1: right back red light
+// 2: red/blue tri-led at the top of the car(police lights)
+// .................................................................................
+// The sensors are:
+// Distance Sensor 1: The sensor at the front. When it detects something, the car starts moving.
+// .................................................................................
+// The servos are:
+//     Rotation:
+// 1: Front left wheel
+// 2: Front right wheel
+//     Position:
+// 3: spinning police LED
 let direction = 0
 hummingbird.startHummingbird()
 direction = 0
 hummingbird.setLED(ThreePort.One, 100)
 hummingbird.setLED(ThreePort.Two, 100)
-serial.writeNumber(hummingbird.getSensor(SensorType.Distance, ThreePort.One))
 // Move the robot if it senses my hand, and stop moving it when it senses my hand again.
 basic.forever(function () {
     if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 7) {
